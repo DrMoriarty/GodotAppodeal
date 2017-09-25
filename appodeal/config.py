db2831196ba3b89233c0a9aadd97694ea9b7c0a2
Vasiliy Makarov <drmoriarty.0@gmail.com>
2017-09-25 10:52:14 +0300

    fix godot building, need to copy settings.gradle to godot/platform/android/java

--- appodeal/config.py
+++ appodeal/config.py
@@ -4,7 +4,10 @@ def can_build(plat):
 def configure(env):
 	if (env['platform'] == 'android'):
 		env.android_add_java_dir("android/src")
+		env.android_add_maven_repository("url 'https://adcolony.bintray.com/AdColony'")
 		env.android_add_to_manifest("android/AndroidManifestChunk.xml")
 		env.android_add_to_permissions("android/AndroidManifestPermissionsChunk.xml")
 		env.android_add_dependency("compile 'com.google.android.gms:play-services-ads:8.4.0'")
+		env.android_add_dependency("compile 'com.adcolony:sdk:3.2.1'")
+		env.android_add_dependency("compile project(':mmedia')")
 		env.android_add_dependency("compile fileTree(dir: '../../../modules/appodeal/android/libs', include: '*.jar')")
